be2dc2191a8eeb4adeb81f5245b652318e31d6fa
Vladimír Antoš <vladimirantos@seznam.cz>
2019-01-27 18:38:25 +0100

    Initialize commit

---
+++ Syrus.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Syrus.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
