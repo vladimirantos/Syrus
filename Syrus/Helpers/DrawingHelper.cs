45f3a06863b2ea52d41534a98cce61aa1dc50718
Vladimír Antoš <vladimirantos@seznam.cz>
2019-02-04 18:39:44 +0100

    Position application window to center

---
+++ Syrus/Helpers/DrawingHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace Syrus.Helpers
+{
+    internal static class DrawingHelper
+    {
+
+        /// <summary>
+        /// Convert pixel to DIP value.
+        /// </summary>
+        public static Point TransformPixelsToDip(Visual visual, double unitX, double unitY)
+        {
+            Matrix matrix;
+            var source = PresentationSource.FromVisual(visual);
+            if (source != null)
+            {
+                matrix = source.CompositionTarget.TransformFromDevice;
+            }
+            else
+            {
+                using (var src = new HwndSource(new HwndSourceParameters()))
+                {
+                    matrix = src.CompositionTarget.TransformFromDevice;
+                }
+            }
+            return new Point((int)(matrix.M11 * unitX), (int)(matrix.M22 * unitY));
+        }
+    }
+}
--- Syrus/MainWindow.xaml.cs
+++ Syrus/MainWindow.xaml.cs
@@ -1,10 +1,40 @@
-﻿namespace Syrus
+﻿using Syrus.Helpers;
+using System;
+using System.Windows;
+using System.Windows.Forms;
+namespace Syrus
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += (s, e) => InitializePosition();
+        }
+
+        private void InitializePosition()
+        {
+            Top = WindowTop();
+            Left = WindowLeft();
+        }
+
+        private double WindowTop()
+        {
+            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+            var dip1 = DrawingHelper.TransformPixelsToDip(this, 0, screen.WorkingArea.Y);
+            var dip2 = DrawingHelper.TransformPixelsToDip(this, 0, screen.WorkingArea.Height);
+            var top = (dip2.Y - ActualHeight) / 4 + dip1.Y;
+            return top;
+        }
+
+        private double WindowLeft()
+        {
+            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+            var dip1 = DrawingHelper.TransformPixelsToDip(this, screen.WorkingArea.X, 0);
+            var dip2 = DrawingHelper.TransformPixelsToDip(this, screen.WorkingArea.Width, 0);
+            var left = (dip2.X - ActualWidth) / 2 + dip1.X;
+            return left;
         }
     }
 }
