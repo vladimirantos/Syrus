42cbaa7de4eb088f6eb055a304b8af3a47cc72a1
Vladimír Antoš <vladimirantos@seznam.cz>
2019-02-06 21:20:36 +0100

    Prepared for searching

--- Syrus.Core/SearchEngine.cs
+++ Syrus.Core/SearchEngine.cs
@@ -1,11 +1,31 @@
-﻿namespace Syrus.Core
+﻿using Syrus.Plugin;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Syrus.Core
 {
     public interface ISearch
     {
-
+        IEnumerable<Result> Search(string match);
     }
 
-    public class SearchEngine : ISearch
+    public class SearchingEngine : ISearch
     {
+        private List<ISearch> _searchEngines;
+
+        public SearchingEngine() => _searchEngines = new List<ISearch>();
+
+        public void Add(ISearch searchEngine) => _searchEngines.Add(searchEngine);
+
+        public async IEnumerable<Result> Search(string match)
+        {
+            Task<IEnumerable<Result>>[] tasks = new Task<IEnumerable<Result>>[_searchEngines.Count];
+            for(int i = 0; i < _searchEngines.Count; i++)
+            {
+                tasks[i] = Task.Factory.StartNew(() => _searchEngines[i].Search(match));
+            }
+            return (await Task.WhenAll(tasks)).ToList();
+        }
     }
 }
