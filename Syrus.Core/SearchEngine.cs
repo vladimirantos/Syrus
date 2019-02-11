9c1e6b504325c9d1e2663bf7820366117f0dbb72
Vladimír Antoš <vladimirantos@seznam.cz>
2019-02-11 22:44:53 +0100

    Indexing plugins before searching

--- Syrus.Core/SearchEngine.cs
+++ Syrus.Core/SearchEngine.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Collections;
 
 namespace Syrus.Core
 {
@@ -15,8 +16,13 @@ namespace Syrus.Core
     public class SearchEngine : ISearch
     {
         private List<ISearch> _searchEngines;
+
+        /// <summary>
+        /// Key is command and value is IPlugin
+        /// </summary>
         private Dictionary<string, IPlugin> _commandPlugins = new Dictionary<string, IPlugin>();
-        private Dictionary<string[], IEnumerable<IPlugin>> _termsPlugins = new Dictionary<string[], IEnumerable<IPlugin>>();
+        private List<KeyValuePair<string, IPlugin>> _termsPlugins = new List<KeyValuePair<string, IPlugin>>();
+
         public IEnumerable<PluginPair> Plugins { get; set; }
 
         public SearchEngine() => _searchEngines = new List<ISearch>();
@@ -28,17 +34,26 @@ namespace Syrus.Core
             foreach(PluginPair p in Plugins)
             {
                 _commandPlugins.Add(p.Metadata.Command, p.Plugin);
+                foreach (string term in p.Metadata.SearchingPatterns)
+                    _termsPlugins.Add(new KeyValuePair<string, IPlugin>(term, p.Plugin));
             }
+            _termsPlugins.Sort(new KeyValuePairComparer());
         }
 
-        public async IEnumerable<Result> Search(string match)
+        public IEnumerable<Result> Search(string match)
         {
             Task<IEnumerable<Result>>[] tasks = new Task<IEnumerable<Result>>[_searchEngines.Count];
             for(int i = 0; i < _searchEngines.Count; i++)
             {
                 tasks[i] = Task.Factory.StartNew(() => _searchEngines[i].Search(match));
             }
-            return (await Task.WhenAll(tasks)).ToList();
+            //return (await Task.WhenAll(tasks)).ToList();
+            return new List<Result>();
         }
+    }
+
+    class KeyValuePairComparer : IComparer<KeyValuePair<string, IPlugin>>
+    {
+        public int Compare(KeyValuePair<string, IPlugin> x, KeyValuePair<string, IPlugin> y) => string.Compare(x.Key, y.Key);
     }
 }
--- Syrus.Test/Program.cs
+++ Syrus.Test/Program.cs
@@ -1,16 +1,35 @@
 ﻿using Syrus.Core;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Syrus.Test
 {
     class Program
     {
+        static List<KeyValuePair<string[], IEnumerable<string>>> _items = new List<KeyValuePair<string[], IEnumerable<string>>>();
+        static Random random = new Random();
         static void Main(string[] args)
         {
-            string instalationFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Syrus");
-            Core.Syrus factory = new Core.Syrus(instalationFolder);
-            factory.LoadPlugins().Initialize();
+            //string instalationFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Syrus");
+            //Core.Syrus factory = new Core.Syrus(instalationFolder);
+            //factory.LoadPlugins().Initialize();
+
+            //_items.Add(new KeyValuePair<string[], IEnumerable<string>>(new[] { "K1", "K2", "K3" }, new List<string>() { "Plugin1" }));
+            //_items.Add(new KeyValuePair<string[], IEnumerable<string>>(new[] { "K4", "K1", "K5" }, new List<string>() { "Plugin2" }));
+            //_items.Add(new KeyValuePair<string[], IEnumerable<string>>(new[] { "K6", "K7", "K4" }, new List<string>() { "Plugin3" }));
+
+            /**
+             * Výsledek
+             * K2, K3 -> Plugin1
+             * K1 -> Plugin1, Plugin2
+             * K5 -> Plugin2
+             * K4 -> Plugin2, Plugin3
+             * K6, K7 -> Plugin 3
+             */
+
         }
     }
 }
