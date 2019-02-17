462f3c1c739b49db9da8a0f6b5fa6c373b759103
Vladimír Antoš <vladimirantos@seznam.cz>
2019-02-17 11:22:41 +0100

    Styled search panel

--- Syrus/ViewModel/SearchingViewModel.cs
+++ Syrus/ViewModel/SearchingViewModel.cs
@@ -1,22 +1,33 @@
-﻿namespace Syrus.ViewModel
+﻿using Syrus.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace Syrus.ViewModel
 {
     class SearchingViewModel : NotifyPropertyChanges
     {
         private string _query;
+        private Core.Syrus _syrus;
+
         public string SearchingQuery 
         {
             get => _query;
             set => SetProperty(ref _query, value, Search);
         }
 
-        public SearchingViewModel()
-        {
+        public ObservableCollection<Result> Results { get; set; }
 
-        }
+        public SearchingViewModel() 
+            => _syrus = new Core.Syrus(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Syrus"));
 
         public void Search(string newValue)
         {
-
+            List<Result> results = _syrus.Search(newValue).ToList();
+            if(results.Count > 0)
+                results.ForEach(Results.Add);
         }
     }
 }
