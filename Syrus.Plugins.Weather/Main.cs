35e90703fd781c712f390bc84946908ecb12fca5
Vladimír Antoš <vladimirantos@seznam.cz>
2019-03-14 21:44:44 +0100

    Fixed icon in weather plugin

--- Syrus.Plugins.Weather/Main.cs
+++ Syrus.Plugins.Weather/Main.cs
@@ -2,12 +2,15 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace Syrus.Plugins.Weather
 {
     public class Main : IPlugin
     {
+        private const string ApiKey = "2747ba3ddb5d7e5e4d358c143ce2f61f";
         public void OnInitialize(PluginContext context)
         {
+            GetCurrentPosition();
         }
 
         public IEnumerable<Result> Search(Query query)
@@ -22,6 +25,11 @@ namespace Syrus.Plugins.Weather
                     QuickResult = $"- {query.Arguments} 19 °C, zataženo"
                 }
             };
+        }
+
+        private void GetCurrentPosition()
+        {
+            //var x = geo.Position;
         }
     }
 }
