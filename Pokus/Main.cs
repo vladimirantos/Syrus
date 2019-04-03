3f0ed6dd69317e91e4a033814a1d788061fab706
Vladimír Antoš <vladimirantos@seznam.cz>
2019-04-03 16:48:43 +0200

    Prepared for displaying usercontrol

---
+++ Pokus/Main.cs
@@ -0,0 +1,29 @@
+﻿using Syrus.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Pokus
+{
+    public class Main : IPlugin
+    {
+        public void OnInitialize(PluginContext context)
+        {
+        }
+
+        public IEnumerable<Result> Search(Query query)
+        {
+            return new List<Result>()
+            {
+                new Result()
+            {
+                Text = "BANIK PICO",
+                Group = "Pičoviny",
+                Content = new DataTemplate()
+            }
+        };
+            
+        }
+    }
+}
