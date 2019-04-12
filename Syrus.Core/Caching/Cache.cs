a8b38474548e555ef8155110d8c118ec99998f51
Vladimír Antoš <vladimirantos@seznam.cz>
2019-04-12 17:53:19 +0200

    Created cache class

--- Syrus.Core/Caching/Cache.cs
+++ Syrus.Core/Caching/Cache.cs
@@ -261,8 +261,81 @@ namespace Syrus.Core.Caching
         }
     }
 
-    internal class Cache<T>
+    internal class Cache<T> : CacheBase<T> where T : class
     {
+        private List<T> _cache = new List<T>();
 
+        public override void Clear()
+        {
+            locker.EnterWriteLock();
+            try
+            {
+                _cache.Clear();
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+
+        public override bool Exists(T value)
+        {
+            if (Disposed)
+                return false;
+            locker.EnterReadLock();
+            try
+            {
+                return _cache.Any(item => item == value);
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+
+        public override void Remove(T key)
+        {
+            if (Disposed)
+                return;
+            locker.EnterWriteLock();
+            try
+            {
+                _cache.Remove(key);
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (Disposed)
+                return;
+            locker.EnterWriteLock();
+            try
+            {
+                _cache.Add(value);
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            if (Disposed)
+                return new List<T>();
+            locker.EnterReadLock();
+            try
+            {
+                return _cache;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
     }
 }
