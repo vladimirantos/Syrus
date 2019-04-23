0cfcee5cf5bedc36ce077191b6b398fe99977730
Vladimír Antoš <vladimirantos@seznam.cz>
2019-04-23 18:37:07 +0200

    Convert color to hsv

--- Syrus.Plugins.HexRgb/Main.cs
+++ Syrus.Plugins.HexRgb/Main.cs
@@ -4,6 +4,7 @@ using Syrus.Plugin;
 using System.Drawing;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Linq;
 
 namespace Syrus.Plugins.HexRgb
 {
@@ -21,61 +22,85 @@ namespace Syrus.Plugins.HexRgb
             var isRgb = _rgbRegex.IsMatch(raw);
             if ((!raw.StartsWith("#") || (raw.Length < 4)) && !isRgb)
                 return Task.FromResult<IEnumerable<Result>>(new List<Result>());
-            return Task.FromResult<IEnumerable<Result>>(!isRgb ? HexToRgb(raw) : RgbToHex(raw));
-        }
-
-        private List<Result> HexToRgb(string hex)
-        {
-            var color = ColorTranslator.FromHtml(hex);
-            var colorString = $"rgb({color.R}, {color.G}, {color.B})";
-            //CreateIcon(color);
-            return new List<Result>
+            List<Result> results = new List<Result>();
+            if (!isRgb)
             {
-                new Result
+                string rgb = HexToRgb(raw);
+                string hsv = RgbToHsv(rgb);
+                results.Add(new Result
                 {
-                    Text = colorString,
+                    Text = $"RGB {rgb}",
                     Group = "Převod do RGB",
                     OnClick = (IAppApi api, Result currentResult) =>
                     {
-                        Clipboard.SetText(colorString);
+                        Clipboard.SetText($"rgb({rgb})");
                     }
-                    //Title = colorString,
-                    //IcoPath = Path.Combine("images", _resultIcon),
-                    //Action = ctx =>
-                    //{
-                    //    Clipboard.SetText(colorString);
-                    //    return true;
-                    //}
-                }
-            };
-        }
-
-        private List<Result> RgbToHex(string rgb)
-        {
-            foreach (Match match in Regex.Matches(rgb, _rgbRegex.ToString(), RegexOptions.IgnoreCase))
-            {
-                var r = int.Parse(match.Groups[1].Value);
-                var g = int.Parse(match.Groups[2].Value);
-                var b = int.Parse(match.Groups[3].Value);
-                var c = Color.FromArgb(255, r, g, b);
-                var color = ColorTranslator.ToHtml(c);
-                //CreateIcon(c);
-
-                return new List<Result>
+                });
+                results.Add(new Result
                 {
-                    new Result
+                    Text = hsv,
+                    Group = "Převod do HSV",
+                    OnClick = (IAppApi api, Result currentResult) =>
                     {
-                        Text = color,
-                        Group = "Převod do HEX",
-                        OnClick = (IAppApi api, Result currentResult) =>
-                        {
-                            Clipboard.SetText(color);
-                        }
+                        Clipboard.SetText(hsv);
                     }
-                };
+                });
             }
-            return new List<Result>();
+            else
+            {
+                string color = RgbToHex(raw);
+                string hsv = RgbToHsv(raw);
+                results.Add(new Result
+                {
+                    Text = color,
+                    Group = "Převod do HEX",
+                    OnClick = (IAppApi api, Result currentResult) =>
+                    {
+                        Clipboard.SetText(color);
+                    }
+                });
+                results.Add(new Result
+                {
+                    Text = hsv,
+                    Group = "Převod do HSV",
+                    OnClick = (IAppApi api, Result currentResult) =>
+                    {
+                        Clipboard.SetText(hsv);
+                    }
+                });
+            }
+
+            return Task.FromResult<IEnumerable<Result>>(results);
         }
+
+        private string HexToRgb(string hex)
+        {
+            var color = ColorTranslator.FromHtml(hex);
+            return $"{color.R}, {color.G}, {color.B}";
+        }
+
+        private string RgbToHex(string rgb)
+        {
+            Match match = Regex.Matches(rgb, _rgbRegex.ToString(), RegexOptions.IgnoreCase).First();
+            var r = int.Parse(match.Groups[1].Value);
+            var g = int.Parse(match.Groups[2].Value);
+            var b = int.Parse(match.Groups[3].Value);
+            var c = Color.FromArgb(255, r, g, b);
+            return ColorTranslator.ToHtml(c);
+        }
+
+        private string RgbToHsv(string rgb)
+        {
+            Match match = Regex.Matches(rgb, _rgbRegex.ToString(), RegexOptions.IgnoreCase).First();
+            var r = int.Parse(match.Groups[1].Value);
+            var g = int.Parse(match.Groups[2].Value);
+            var b = int.Parse(match.Groups[3].Value);
+            Color color = Color.FromArgb(r, g, b);
+            float hue = color.GetHue();
+            float saturation = color.GetSaturation();
+            float lightness = color.GetBrightness();
+            return $"{hue}, {saturation}, {lightness}";
+         }
 
     }
 }
