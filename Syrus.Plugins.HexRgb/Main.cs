1ac7292c277c0b7c689166b5b01fb93f801e8264
Vladimír Antoš <vladimirantos@seznam.cz>
2019-04-23 17:34:41 +0200

    Created plugin for RGB to HEX

---
+++ Syrus.Plugins.HexRgb/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Syrus.Plugin;
+
+namespace Syrus.Plugins.HexRgb
+{
+    public class Main : IPlugin
+    {
+        public void OnInitialize(PluginContext context)
+        {
+        }
+
+        public Task<IEnumerable<Result>> SearchAsync(Query query)
+        {
+            return Task.FromResult<IEnumerable<Result>>(new List<Result>()
+            {
+                new Result()
+                {
+                    Text = query.Original
+                }
+            });
+        }
+    }
+}
