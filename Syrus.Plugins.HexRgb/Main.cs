3c183da34b13175941f16e29db3e50912d712098
Vladimír Antoš <vladimirantos@seznam.cz>
2019-04-28 18:10:13 +0200

    Cleaned code

--- Syrus.Plugins.HexRgb/Main.cs
+++ Syrus.Plugins.HexRgb/Main.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Linq;
+using System;
 
 namespace Syrus.Plugins.HexRgb
 {
@@ -25,76 +26,51 @@ namespace Syrus.Plugins.HexRgb
             List<Result> results = new List<Result>();
             if (!isRgb)
             {
-                string rgb = HexToRgb(raw);
-                string hsv = RgbToHsv(rgb);
-                results.Add(new Result
+                var color = ColorTranslator.FromHtml(raw);
+                string rgb = HexToRgb(color);
+                string hsv = RgbToHsv(color.R, color.G, color.B);
+
+                results.Add(CreateResult($"RGB {rgb}", "Převod do RGB", (IAppApi api, Result currentResult) =>
                 {
-                    Text = $"RGB {rgb}",
-                    Group = "Převod do RGB",
-                    OnClick = (IAppApi api, Result currentResult) =>
-                    {
-                        Clipboard.SetText($"rgb({rgb})");
-                    }
-                });
-                results.Add(new Result
+                    Clipboard.SetText($"rgb({rgb})");
+                }));
+
+                results.Add(CreateResult(hsv, "Převod do HSV", (IAppApi api, Result currentResult) =>
                 {
-                    Text = hsv,
-                    Group = "Převod do HSV",
-                    OnClick = (IAppApi api, Result currentResult) =>
-                    {
-                        Clipboard.SetText(hsv);
-                    }
-                });
+                    Clipboard.SetText(hsv);
+                }));
             }
             else
             {
-                string color = RgbToHex(raw);
-                string hsv = RgbToHsv(raw);
-                results.Add(new Result
+                Match match = Regex.Matches(raw, _rgbRegex.ToString(), RegexOptions.IgnoreCase).First();
+                var r = int.Parse(match.Groups[1].Value);
+                var g = int.Parse(match.Groups[2].Value);
+                var b = int.Parse(match.Groups[3].Value);
+                string color = RgbToHex(r, g, b);
+                string hsv = RgbToHsv(r, g, b);
+                results.Add(CreateResult(color, "Převod do HEX", (IAppApi api, Result currentResult) =>
                 {
-                    Text = color,
-                    Group = "Převod do HEX",
-                    OnClick = (IAppApi api, Result currentResult) =>
-                    {
-                        Clipboard.SetText(color);
-                    }
-                });
-                results.Add(new Result
+                    Clipboard.SetText(color);
+                }));
+                results.Add(CreateResult(hsv, "Převod do HSV", (IAppApi api, Result currentResult) =>
                 {
-                    Text = hsv,
-                    Group = "Převod do HSV",
-                    OnClick = (IAppApi api, Result currentResult) =>
-                    {
-                        Clipboard.SetText(hsv);
-                    }
-                });
+                    Clipboard.SetText(hsv);
+                }));
             }
 
             return Task.FromResult<IEnumerable<Result>>(results);
         }
 
-        private string HexToRgb(string hex)
-        {
-            var color = ColorTranslator.FromHtml(hex);
-            return $"{color.R}, {color.G}, {color.B}";
-        }
+        private string HexToRgb(Color color) => $"{color.R}, {color.G}, {color.B}";
 
-        private string RgbToHex(string rgb)
+        private string RgbToHex(int r, int g, int b)
         {
-            Match match = Regex.Matches(rgb, _rgbRegex.ToString(), RegexOptions.IgnoreCase).First();
-            var r = int.Parse(match.Groups[1].Value);
-            var g = int.Parse(match.Groups[2].Value);
-            var b = int.Parse(match.Groups[3].Value);
             var c = Color.FromArgb(255, r, g, b);
             return ColorTranslator.ToHtml(c);
         }
 
-        private string RgbToHsv(string rgb)
+        private string RgbToHsv(int r, int g, int b)
         {
-            Match match = Regex.Matches(rgb, _rgbRegex.ToString(), RegexOptions.IgnoreCase).First();
-            var r = int.Parse(match.Groups[1].Value);
-            var g = int.Parse(match.Groups[2].Value);
-            var b = int.Parse(match.Groups[3].Value);
             Color color = Color.FromArgb(r, g, b);
             float hue = color.GetHue();
             float saturation = color.GetSaturation();
@@ -102,5 +78,13 @@ namespace Syrus.Plugins.HexRgb
             return $"{hue}, {saturation}, {lightness}";
          }
 
+
+        private Result CreateResult(string text, string group, Action<IAppApi, Result> onClick)
+            => new Result()
+            {
+                Text = text,
+                Group = group,
+                OnClick = onClick
+            };
     }
 }
