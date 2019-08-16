fa7e01432d676061b04a439fb6bce99b991be03d
Vladimír Antoš <vladimirantos@seznam.cz>
2019-08-16 18:49:18 +0200

    Created plugin for text operations

---
+++ Syrus.Plugins.Text/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Syrus.Plugin;
+
+namespace Syrus.Plugins.Text
+{
+    public class Main : IPlugin
+    {
+        public void OnInitialize(PluginContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Result>> SearchAsync(Query query)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
