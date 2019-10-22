bb9ef263af1cda518de4be395ecaf00203b68d03
Vladimír Antoš <vladimirantos@seznam.cz>
2019-10-22 19:32:58 +0200

    Created HttpClient class

---
+++ Syrus.Utils/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syrus.Utils
+{
+    public class SyrusException : Exception
+    {
+        public SyrusException(): base()
+        {
+
+        }
+
+        public SyrusException(string message): base(message)
+        {
+
+        }
+
+        public SyrusException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ Syrus.Utils/Http/Http.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Syrus.Utils.Http
+{
+    public static class Http
+    {
+        private const string UserAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36";
+        private static readonly HttpClientHandler _clientHandler = new HttpClientHandler();
+        
+        private static readonly Lazy<HttpClient> _lazyInstance = new Lazy<HttpClient>(() => new HttpClient(_clientHandler, false));
+        private static HttpClient HttpClient => _lazyInstance.Value;
+
+        static Http()
+        {
+            HttpClient.DefaultRequestHeaders.ConnectionClose = false;
+            HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+        }
+
+        public static async Task<string> Get(string url)
+        {
+            Logging.Log.Debug($"HTTP GET: {url}");
+            try
+            {
+                var response = await HttpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (HttpRequestException e)
+            {
+                throw new SyrusException($"HTTP GET {url} throws exception.", e);
+            }
+        }
+    }
+}
