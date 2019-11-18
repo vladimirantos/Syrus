9d6e1be3a986bc57b3d7555ebfedc660f91b0645
Vladimír Antoš <vladimirantos@seznam.cz>
2019-11-18 22:48:31 +0100

    Added Plugins.Files

---
+++ Syrus.Plugins.Files/Main.cs
@@ -0,0 +1,20 @@
+﻿using Syrus.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syrus.Plugins.Files
+{
+    public class Main : BasePlugin
+    {
+        public override void OnInitialize(PluginContext context)
+        {
+        }
+
+        public async override Task<IEnumerable<Result>> SearchAsync(Query query)
+        {
+            return Empty;
+        }
+    }
+}
